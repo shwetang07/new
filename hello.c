3b488d0f1dd37d096ea9f5dca8e6372d152c2fc9
shwetang07 <30969458+shwetang07@users.noreply.github.com>
2019-10-31 16:34:06 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main(){
+int a,b,c;
+a=10;
+b=10;
+c=a+b;
+
+   printf("%d\n",c);
+
+    int n = 42;
+    printf("The answer to life, universe and everything is %d\n", n);
+
+    printf("Hello\tWorld\n\n");
+
+
+
+
+   }
+
